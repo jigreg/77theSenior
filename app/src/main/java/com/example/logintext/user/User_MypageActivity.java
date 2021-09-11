1989dc83fd1b08940ddd0386fe448259a51b1d89
jigreg <jigreg@naver.com>
2021-09-11 23:00:05 +0900

    ADD Mypage xml & java(add protector info dialog)

---
+++ app/src/main/java/com/example/logintext/user/User_MypageActivity.java
@@ -0,0 +1,129 @@
+package com.example.logintext.user;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.logintext.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class User_MypageActivity extends AppCompatActivity {
+
+    private ImageButton back;
+    private Button proinfo;
+    private TextView allrank,allpercent,walkrank,walkpercent,brainrank,brainpercent,grade;
+    private String uid, proname, prophonenum, prouid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_mypage);
+
+        back = (ImageButton)findViewById(R.id.back);
+        proinfo = (Button)findViewById(R.id.proinfo);
+        allrank = (TextView)findViewById(R.id.allrank);
+        allpercent = (TextView)findViewById(R.id.allpercent);
+        walkrank = (TextView)findViewById(R.id.walkrank);
+        walkpercent = (TextView)findViewById(R.id.walkpercent);
+        brainrank = (TextView)findViewById(R.id.brainrank);
+        brainpercent = (TextView)findViewById(R.id.brainpercent);
+        grade = (TextView)findViewById(R.id.grade);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = user.getUid();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference nreference = database.getReference("Users").child("user");
+        DatabaseReference mreference = database.getReference("Users").child("protector");
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(User_MypageActivity.this, User_MainActivity.class));
+                finish();
+            }
+        });
+
+        proinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nreference.child(uid).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        prouid = snapshot.child("myProtector").getValue().toString();
+                        if (prouid.equals("none")) {
+                            prosetDialog();
+                        } else {
+                            mreference.child(prouid).addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    proname = snapshot.child("name").getValue().toString();
+                                    prophonenum = snapshot.child("phonenum").getValue().toString();
+                                    proinfoDialog();
+                                }
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+            }
+        });
+
+
+
+    }//oncreate end
+
+    //알림창
+    void proinfoDialog() {
+        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(User_MypageActivity.this)
+                .setTitle("보호자 정보")
+                .setMessage("보호자 성함은 " + proname + " 입니다." +"\n"+ "보호자 전화번호는 " + prophonenum + " 입니다.")
+                .setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+        AlertDialog msgDlg = msgBuilder.create();
+        msgDlg.show();
+
+    }
+    void prosetDialog() {
+        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(User_MypageActivity.this)
+                .setTitle("보호자 등록")
+                .setMessage("보호자가 등록되지 않았습니다."+"\n"+"확인을 누르면 보호자 등록화면으로 이동됩니다.")
+                .setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                startActivity(new Intent(User_MypageActivity.this, User_RegisterCodeActivity.class));
+                                finish();
+                            }
+                        });
+        AlertDialog msgDlg = msgBuilder.create();
+        msgDlg.show();
+
+    }
+}//main end
