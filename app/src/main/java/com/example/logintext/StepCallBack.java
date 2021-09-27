fa87f0cd0e1b5dff33f25f0056a66518c53de3de
LAPTOP-9JLTL99A\fgvb0 <fgvb017@gmail.com>
2021-09-27 11:20:13 +0900

    watermelon game

--- app/src/main/java/com/example/logintext/StepCallBack.java
+++
@@ -1,6 +0,0 @@
-package com.example.logintext;
-
-public interface StepCallBack {
-    void onStepCallback(int step); // 서비스에서 스탭 변화시 액티비티로 전달하는 콜백 함수
-    void onUnbindService(); // 서비스 언바인드 시 액티비티로 전달하는 콜백 함수
-}
