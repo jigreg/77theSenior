5533df9a826657f093971a92122a26f839ad8f60
jigreg <jigreg@naver.com>
2021-10-23 13:34:06 +0900

    'diagnosis'

---
+++ 77project/app/src/main/java/com/example/logintext/user/User_DiagnosisActivity.java
@@ -0,0 +1,173 @@
+package com.example.logintext.user;
+
+import android.app.TabActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.logintext.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@SuppressWarnings("deprecation")
+public class User_DiagnosisActivity extends TabActivity {
+
+    private ImageButton back;
+    private LinearLayout timeCountSettingLV, timeCountLV;
+    private EditText hourET, minuteET, secondET;
+    private TextView hourTV, minuteTV, secondTV, finishTV;
+    private Button startBtn;
+    private int hour, minute, second;
+
+    private TabHost tabHost;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_diagnosis);
+
+        tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecWalk = tabHost.newTabSpec("WALKING").setIndicator("걸음별");
+        tabSpecWalk.setContent(R.id.walking);
+        tabHost.addTab(tabSpecWalk);
+
+        TabHost.TabSpec tabSpecTrain = tabHost.newTabSpec("TRAINING").setIndicator("점수별");
+        tabSpecTrain.setContent(R.id.training);
+        tabHost.addTab(tabSpecTrain);
+
+        tabHost.setCurrentTab(0);
+
+        back = (ImageButton) findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(User_DiagnosisActivity.this, User_MainActivity.class));
+                finish();
+            }
+        });
+
+        timeCountSettingLV = (LinearLayout)findViewById(R.id.timeCountSettingLV);
+        timeCountLV = (LinearLayout)findViewById(R.id.timeCountLV);
+
+        hourET = (EditText)findViewById(R.id.hourET);
+        minuteET = (EditText)findViewById(R.id.minuteET);
+        secondET = (EditText)findViewById(R.id.secondET);
+
+        hourTV = (TextView)findViewById(R.id.hourTV);
+        minuteTV = (TextView)findViewById(R.id.minuteTV);
+        secondTV = (TextView)findViewById(R.id.secondTV);
+        finishTV = (TextView)findViewById(R.id.finishTV);
+
+        startBtn = (Button)findViewById(R.id.startBtn);
+
+        // 시작버튼 이벤트 1처리
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                timeCountSettingLV.setVisibility(View.GONE);
+                timeCountLV.setVisibility(View.VISIBLE);
+
+                hourTV.setText(hourET.getText().toString());
+                minuteTV.setText(minuteET.getText().toString());
+                secondTV.setText(secondET.getText().toString());
+
+                hour = Integer.parseInt(hourET.getText().toString());
+                minute = Integer.parseInt(minuteET.getText().toString());
+                second = Integer.parseInt(secondET.getText().toString());
+
+                Timer timer = new Timer();
+                TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        // 반복실행할 구문
+
+                        // 0초 이상이면
+                        if(second != 0) {
+                            //1초씩 감소
+                            second--;
+
+                            // 0분 이상이면
+                        } else if(minute != 0) {
+                            // 1분 = 60초
+                            second = 60;
+                            second--;
+                            minute--;
+
+                            // 0시간 이상이면
+                        } else if(hour != 0) {
+                            // 1시간 = 60분
+                            second = 60;
+                            minute = 60;
+                            second--;
+                            minute--;
+                            hour--;
+                        }
+
+                        //시, 분, 초가 10이하(한자리수) 라면
+                        // 숫자 앞에 0을 붙인다 ( 8 -> 08 )
+                        if(second <= 9){
+                            secondTV.setText("0" + second);
+                        } else {
+                            secondTV.setText(Integer.toString(second));
+                        }
+
+                        if(minute <= 9){
+                            minuteTV.setText("0" + minute);
+                        } else {
+                            minuteTV.setText(Integer.toString(minute));
+                        }
+
+                        if(hour <= 9){
+                            hourTV.setText("0" + hour);
+                        } else {
+                            hourTV.setText(Integer.toString(hour));
+                        }
+
+                        // 시분초가 다 0이라면 toast를 띄우고 타이머를 종료한다..
+                        if(hour == 0 && minute == 0 && second == 0) {
+                            timer.cancel();//타이머 종료
+                            finishTV.setText("타이머가 종료되었습니다.");
+                        }
+                    }
+                };
+
+                //타이머를 실행
+                timer.schedule(timerTask, 0, 1000); //Timer 실행
+            }
+        });
+
+
+    }//oncreate end
+}//final end
